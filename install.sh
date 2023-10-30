bb6e9a394647a3ebeab7dc2db23e6f4da2a0ec93
RafaelKtistakis <rafaelktistakis@gmail.com>
2023-10-30 19:21:23 +0000

    Core, installing/removing packages through vars

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 
 sudo apt update && sudo apt install -y python3-dev python3-pip libyaml-dev libffi-dev git && sudo pip3 install --no-binary pyyaml ansible
 ansible-galaxy install -r requirements.yml
-ansible-galaxy collection install ./rafaelktistakis
+ansible-galaxy collection install ./rafaelktistakis --force
 ansible-playbook main.yml
