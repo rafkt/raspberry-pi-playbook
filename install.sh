ca869b54e873799f03a877977b0f80d5af57c94b
RafaelKtistakis <rafaelktistakis@gmail.com>
2023-10-31 18:41:54 +0000

    Adding target hosts through Ansible

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install ansible packages or update them
-
-sudo apt update && sudo apt install -y python3-dev python3-pip libyaml-dev libffi-dev git && sudo pip3 install --no-binary pyyaml ansible
+# In Debian 12 externall packages e.g. from pip can't be mixed with internal. Use pipx to have this managed or --break-system-packages
+sudo apt update && sudo apt install -y python3-dev python3-pip libyaml-dev libffi-dev git pipx && sudo pip3 install --no-binary pyyaml ansible
 ansible-galaxy install -r requirements.yml
 ansible-galaxy collection install ./rafaelktistakis --force
-ansible-playbook main.yml
+ansible-playbook main.yml --ask-pass --ask-become-pass
