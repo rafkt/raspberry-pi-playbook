5a61e817ff549a6895c39a2f1065bd03d50eb519
RafaelKtistakis <rafaelktistakis@gmail.com>
2023-10-31 19:51:51 +0000

    Added support for vault. Further configuration through ansible.cfg. Default editor nano for zsh

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@
 sudo apt update && sudo apt install -y python3-dev python3-pip libyaml-dev libffi-dev git pipx && sudo pip3 install --no-binary pyyaml ansible
 ansible-galaxy install -r requirements.yml
 ansible-galaxy collection install ./rafaelktistakis --force
-ansible-playbook main.yml --ask-pass --ask-become-pass
+ansible-playbook main.yml
+
+# For flexibility although bad practise, you can disable sudo password for target user.
+# sudo visudo /etc/sudoers.d/010_pi-nopasswd
+# If in bullseye
+# or 
+# sudo visudo
+# if newer debian
+# Add user ALL=(ALL) NOPASSWD: ALL
+# or revert to user ALL=(ALL) ALL
+# Alternatively:
+# ansible-vault create vault.yml
